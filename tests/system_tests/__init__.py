08f7c4c672b59c458dbf8032bf71f89037cdb130
theo <theo.renauxv@gmail.com>
2025-10-29 17:34:05 +0100

    clean main

--- tests/system_tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-import sys
-from os.path import dirname, join, normpath
-
-THIS_DIR = dirname(__file__)
-PROJ_DIR = normpath(join(THIS_DIR, '..', '..', 'project'))
-sys.path.append(PROJ_DIR)
